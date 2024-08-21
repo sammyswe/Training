c6bd411a3dab0233236dc3a8bf1283c651491038
Sam <samfelliott1@gmail.com>
2024-08-21 12:01:16 +0100

    Phrase Printer function

---
+++ functions_and_scope/phrasePrinter.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+void printPhrase(char string[]); 
+
+int main(void){
+
+    char printString[] = "Print this string";
+
+    printPhrase(printString);
+
+}
+
+void printPhrase(char string[]){
+
+    for (int i = 0; string[i] != '\0'; i++){
+        printf("%c", string[i]);
+    }
+   printf("\n"); 
+}
--- logicalStatements/iterativeABPrinting.c
+++ logicalStatements/iterativeABPrinting.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stlib.h>
 
 int main(void){
 
@@ -27,4 +28,6 @@ int main(void){
 
 
     }
+
+    return EXIT_SUCCESS;
 }
